fef90b967d3116f2df48a9493a00f8305041103f
Hunter Vaccaro <hv@Hunters-MacBook-Pro.local>
2022-03-26 00:10:31 -0400

    updated readme file

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -29,7 +29,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = 30
         
         query.findObjectsInBackground { (posts, error) in
             if(posts != nil) {
